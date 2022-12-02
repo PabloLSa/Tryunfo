1d6658f4dd8c5c4c59f2f629d30a7381ee60d448
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-12-01 21:03:53 -0300

    2. Adicionando as props necessárias ao componente de formulário

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      // hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
     return (
       <div>
         <input
@@ -9,47 +24,95 @@ class Form extends React.Component {
           name=""
           id=""
           data-testid="name-input"
+          value={ cardName }
+          onChange={ onInputChange }
         />
         <input
           type="textarea"
           name=""
           id=""
           data-testid="description-input"
+          value={ cardDescription }
+          onChange={ onInputChange }
         />
         <input
           type="number"
           name=""
           id=""
           data-testid="attr1-input"
+          value={ cardAttr1 }
+          onChange={ onInputChange }
         />
         <input
           type="number"
           name=""
           id=""
           data-testid="attr2-input"
+          value={ cardAttr2 }
+          onChange={ onInputChange }
         />
         <input
           type="number"
           name=""
           id=""
           data-testid="attr3-input"
+          value={ cardAttr3 }
+          onChange={ onInputChange }
         />
         <input
           type="text"
           name=""
           id=""
           data-testid="image-input"
+          value={ cardImage }
+          onChange={ onInputChange }
         />
-        <select name="" id="" data-testid="rare-input">
+        <select
+          name=""
+          id=""
+          data-testid="rare-input"
+          value={ cardRare }
+          onChange={ onInputChange }
+        >
           <option value="normal">normal</option>
           <option value="raro">normal</option>
           <option value="muito raro">normal</option>
         </select>
-        <input type="checkbox" name="" id="" data-testid="trunfo-input" />
-        <button type="button" data-testid="save-button">Salvar</button>
+        <input
+          type="checkbox"
+          name=""
+          id=""
+          data-testid="trunfo-input"
+          checked={ cardTrunfo }
+          onChange={ onInputChange }
+        />
+        <button
+          type="button"
+          data-testid="save-button"
+          onClick={ onSaveButtonClick }
+          disabled={ isSaveButtonDisabled }
+        >
+          Salvar
+
+        </button>
 
       </div>
     );
   }
 }
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  //   hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+
+};
 export default Form;
