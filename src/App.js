cd18b7b32be3c41e9452b4c3ecd5a901e199b4d1
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-12-03 23:25:33 -0300

    corrigir

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '',
+    cardAttr2: '',
+    cardAttr3: '',
+    cardImage: '',
+    cardRare: '',
+    cardTrunfo: false,
+    isSaveButtonDisabled: true,
+  };
+
+  onInputChange = ({ target }) => {
+    const { value, name } = target;
+    // console.log('O nome campo é', name);
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
-        <Card />
+        <Form { ...this.state } onInputChange={ this.onInputChange } />
+        <Card { ...this.state } />
       </div>
     );
   }
